790eb0f958abb75b8d79ba8e6056b0639da60237
Noor Eldeen Salah <m-2123325228@s-mu.edu.eg>
2021-08-23 09:27:52 +0200

    Format project files with prettier

--- src/sql-query.ts
+++ src/sql-query.ts
@@ -1,30 +1,30 @@
 import { createDbWorker } from "sql.js-httpvfs";
 
 const workerUrl = new URL(
-    "sql.js-httpvfs/dist/sqlite.worker.js",
-    import.meta.url
+  "sql.js-httpvfs/dist/sqlite.worker.js",
+  import.meta.url
 );
 const wasmUrl = new URL("sql.js-httpvfs/dist/sql-wasm.wasm", import.meta.url);
 
 async function load(sqlQuery) {
-    const worker = await createDbWorker(
+  const worker = await createDbWorker(
     [
-        {
+      {
         from: "inline",
         config: {
-            serverMode: "full",
-            url: "/database.db",
-            requestChunkSize: 4096,
+          serverMode: "full",
+          url: "/database.db",
+          requestChunkSize: 4096,
         },
-        },
+      },
     ],
     workerUrl.toString(),
     wasmUrl.toString()
-    );
+  );
 
   const result = await worker.db.query(sqlQuery);
 
-    document.body.textContent = JSON.stringify(result);
+  document.body.textContent = JSON.stringify(result);
 }
 
-export default load
+export default load;
--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,69 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const path = require('path');
-const sveltePreprocess = require('svelte-preprocess');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const sveltePreprocess = require("svelte-preprocess");
 
-const mode = process.env.NODE_ENV || 'development';
-const prod = mode === 'production';
+const mode = process.env.NODE_ENV || "development";
+const prod = mode === "production";
 
 module.exports = {
-	entry: {
-		'bundle': ['./src/main.ts']
-	},
-	resolve: {
-		alias: {
-			svelte: path.dirname(require.resolve('svelte/package.json'))
-		},
-		extensions: ['.mjs', '.js', '.ts', '.svelte'],
-		mainFields: ['svelte', 'browser', 'module', 'main']
-	},
-	output: {
-		path: path.join(__dirname, '/build'),
-		filename: '[name].js',
-		chunkFilename: '[name].[id].js'
-	},
-	module: {
-			rules: [
-				{
-					test: /\.ts$/,
-					loader: 'ts-loader',
-					exclude: /node_modules/
-				},
-				{
-				test: /\.svelte$/,
-				use: {
-					loader: 'svelte-loader',
-					options: {
-						compilerOptions: {
-							dev: !prod
-						},
-						emitCss: prod,
-						hotReload: !prod,
-							preprocess: sveltePreprocess({ sourceMap: !prod })
-					}
-				}
-			},
-			{
-				test: /\.css$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					'css-loader'
-				]
-			},
-			{
-				// required to prevent errors from Svelte on Webpack 5+
-				test: /node_modules\/svelte\/.*\.mjs$/,
-				resolve: {
-					fullySpecified: false
-				}
-			}
-		]
-	},
-	mode,
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: '[name].css'
-		})
-	],
-	devtool: prod ? false : 'source-map',
-	devServer: {
-		hot: true
-	}
+  entry: {
+    bundle: ["./src/main.ts"],
+  },
+  resolve: {
+    alias: {
+      svelte: path.dirname(require.resolve("svelte/package.json")),
+    },
+    extensions: [".mjs", ".js", ".ts", ".svelte", ".tsx"],
+    mainFields: ["svelte", "browser", "module", "main"],
+  },
+  output: {
+    path: path.join(__dirname, "/build"),
+    filename: "[name].js",
+    chunkFilename: "[name].[id].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.svelte$/,
+        use: {
+          loader: "svelte-loader",
+          options: {
+            compilerOptions: {
+              dev: !prod,
+            },
+            emitCss: prod,
+            hotReload: !prod,
+            preprocess: sveltePreprocess({ sourceMap: !prod }),
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        // required to prevent errors from Svelte on Webpack 5+
+        test: /node_modules\/svelte\/.*\.mjs$/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+    ],
+  },
+  mode,
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+  devtool: prod ? false : "source-map",
+  devServer: {
+    hot: true,
+    publicPath: "/build",
+  },
 };
