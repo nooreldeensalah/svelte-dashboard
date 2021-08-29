81140a677e17924d22090cb1a5663f0dcd5766ec
Noor Eldeen Salah <m-2123325228@s-mu.edu.eg>
2021-08-29 21:34:18 +0200

    Install and configure rollup-plugin-copy

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
+import copy from 'rollup-plugin-copy';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,6 +8,23 @@ const config = {
 	// for more information about preprocessors
 	preprocess: preprocess(),
 	kit: {
+		vite: {
+			plugins: [
+				copy({
+					targets: [
+						{
+							src: [
+								'./node_modules/sql.js-httpvfs/dist/sqlite.worker.js',
+								'./node_modules/sql.js-httpvfs/dist/sql-wasm.wasm'
+							],
+							dest: './static'
+						}
+					],
+					verbose: true,
+					hook: 'buildStart' // Required for dev mode.
+				})
+			]
+		},
 		adapter: adapter(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
